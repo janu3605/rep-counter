530d5b1a7d6fa36497ef0cbf012757956140f93d
janu3605 <janu3605a@gmail.com>
2025-08-07 15:02:53 +0530

    starting steps

---
+++ src/Components/Timer.jsx
@@ -0,0 +1,18 @@
+import "../Stylings/Timer.css"
+
+function Timer() {
+    return (
+        <div className="container">
+            {/* <button className="cross">*</button>
+            <p>Hello World</p> */}
+            <div className="digit-wrapper">
+                <div className="digit">1</div>
+                <div className="digit">1</div>
+                <div className="digit">1</div>
+                <div className="digit">1</div>
+                <div className="digit">1</div>
+            </div>
+        </div>
+    );
+}
+export default Timer;
